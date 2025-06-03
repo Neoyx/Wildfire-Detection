7930aa475bbe877000745fcd1f8cde43d0a944cf
Neoyx <noah.leuft@gmail.com>
2025-06-03 15:48:30 +0200

    implemented cloudmask

--- wildfire-detection.py
+++ wildfire-detection.py
@@ -4,14 +4,16 @@ import matplotlib.pyplot as plt
 from math import sqrt, floor
 
 # Pfade zu den jp2-Dateien (Infrarot)
-b12_path = "images/img2/infrared/T14ULG_20250530T174931_B12_20m.jp2"
-b11_path = "images/img2/infrared/T14ULG_20250530T174931_B11_20m.jp2"
-b8a_path = "images/img2/infrared/T14ULG_20250530T174931_B8a_20m.jp2"
+b12_path = "images/2025_Flin_Flon/infrared/T13UFA_20250602T175931_B12_20m.jp2"
+b11_path = "images/2025_Flin_Flon/infrared/T13UFA_20250602T175931_B11_20m.jp2"
+b8a_path = "images/2025_Flin_Flon/infrared/T13UFA_20250602T175931_B8a_20m.jp2"
 
 # Pfade zu den jp2-Dateien (True-Color)
-b04_path = "images/img2/color/T14ULG_20250530T174931_B04_20m.jp2"
-b03_path = "images/img2/color/T14ULG_20250530T174931_B03_20m.jp2"
-b02_path = "images/img2/color/T14ULG_20250530T174931_B02_20m.jp2"
+b04_path = "images/2025_Flin_Flon/color/T13UFA_20250602T175931_B04_20m.jp2"
+b03_path = "images/2025_Flin_Flon/color/T13UFA_20250602T175931_B03_20m.jp2"
+b02_path = "images/2025_Flin_Flon/color/T13UFA_20250602T175931_B02_20m.jp2"
+
+cm_path = "images/2025_Flin_Flon/MSK_CLDPRB_20m.jp2"
 
 # Lade die Bänder (alle 20m → gleiche Form)
 def load_band(path):
@@ -25,6 +27,8 @@ b8a = load_band(b8a_path)
 b04 = load_band(b04_path)
 b03 = load_band(b03_path)
 b02 = load_band(b02_path)
+
+cloud_mask = load_band(cm_path)
 
 # Normalisiere Bänder auf Wertebereich zwischen 0 und 1
 def normalize_band(band):
@@ -40,6 +44,8 @@ b04_norm = normalize_band(b04)
 b03_norm = normalize_band(b03)
 b02_norm = normalize_band(b02)
 
+cloud_mask = normalize_band(cloud_mask)
+
 # Stacking und Normalisierung des Farbbildes
 def stack_img(band_1, band_2, band_3):
     img = np.dstack((band_1, band_2, band_3)) # Stacking, um aus 3 Graustufenbildern ein Farbbild zu machen
@@ -51,8 +57,9 @@ infrared = stack_img(b12_norm, b11_norm, b8a_norm)
 color = stack_img(b04_norm, b03_norm, b02_norm)
 
 # Detektion des Feuers
-threshold = 0.9
+threshold = 0.5
 fire_mask = b12_norm > threshold # Schaue nur nach B12-Band, da Feuer in diesem Wellenlängenbereich am stärksten reflektiert
+fire_mask = fire_mask & (cloud_mask == 0)
 fire_indices = np.where(fire_mask)
 
 # Markierung des Feuers im Farbbild in rot
