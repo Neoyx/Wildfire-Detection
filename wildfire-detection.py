1a570d8bbc65babe9295cff696be17627217ddaf
JonasSeifried <69268825+JonasSeifried@users.noreply.github.com>
2025-06-10 15:30:23 +0200

    added better subplot logic

--- wildfire-detection.py
+++ wildfire-detection.py
@@ -2,9 +2,10 @@ import rasterio
 import numpy as np
 import matplotlib.pyplot as plt
 from math import sqrt, floor
+from dataclasses import dataclass
 
-img_path = "2025_Cape_City_Mountain_Small_Wildfire"
-img_name = "T34HBH_20250223T081839"
+img_path = "2025_Malibu_Wildfire"
+img_name = "T11SLT_20250107T183649"
 
 # Pfade zu den jp2-Dateien (Infrarot)
 b12_path = f"images/{img_path}/infrared/{img_name}_B12_20m.jp2"
@@ -16,7 +17,7 @@ b04_path = f"images/{img_path}/color/{img_name}_B04_20m.jp2"
 b03_path = f"images/{img_path}/color/{img_name}_B03_20m.jp2"
 b02_path = f"images/{img_path}/color/{img_name}_B02_20m.jp2"
 
-cm_path = f"images/{img_path}/MSK_CLDPRB_20m.jp2"
+# cm_path = f"images/{img_path}/MSK_CLDPRB_20m.jp2"
 
 # Lade die Bänder (alle 20m → gleiche Form)
 def load_band(path):
@@ -31,7 +32,7 @@ b04 = load_band(b04_path)
 b03 = load_band(b03_path)
 b02 = load_band(b02_path)
 
-cloud_mask = load_band(cm_path)
+# cloud_mask = load_band(cm_path)
 
 # Normalisiere Bänder auf Wertebereich zwischen 0 und 1
 def normalize_band(band):
@@ -47,7 +48,7 @@ b04_norm = normalize_band(b04)
 b03_norm = normalize_band(b03)
 b02_norm = normalize_band(b02)
 
-cloud_mask = normalize_band(cloud_mask)
+# cloud_mask = normalize_band(cloud_mask)
 
 # Stacking und Normalisierung des Farbbildes
 def stack_img(band_1, band_2, band_3):
@@ -57,6 +58,9 @@ def stack_img(band_1, band_2, band_3):
     return img
 
 infrared = stack_img(b12_norm, b11_norm, b8a_norm)
+swir_composite = stack_img(b12_norm, b8a_norm, b04_norm) # SWIR Composite
+nir_swir_composite = stack_img(b8a_norm, b11_norm, b04_norm) # NIR-SWIR Composite
+
 color = stack_img(b04_norm, b03_norm, b02_norm)
 
 # Detektion des Feuers
@@ -71,24 +75,38 @@ fire_indices = np.where(fire_mask)
 color_marked = color.copy()
 color_marked[fire_indices[0], fire_indices[1]] = [1, 0, 0]
 
-print(fire_indices)
 
 # TODO: rot markierten Feuer-Pixel im Farbbild evtl. durch Dilatation oder andere Filter vergroessern
 
+
+fullscreen = True
+
+@dataclass
+class Subplot:
+    title: str
+    img: np.ndarray
+    cmap: str | None = None
+
+subplots = [
+    Subplot("Farbbild (makiert) (B04, B03, B02 – 20m)", color_marked),
+    Subplot("Infrarotbild (B12, B11, B8A – 20m)", infrared),
+    Subplot("Aktive Feuer-Pixel (weiß)", fire_mask, cmap='gray'),
+    Subplot("Farbbild (B04, B03, B02 – 20m)", color),
+    Subplot("SWIR Composite (B12, B8A, B04)", swir_composite),
+    Subplot("NIR-SWIR Composite (B8A, B11, B04)", nir_swir_composite),
+]
+
 # Visualisierung
-plt.figure(figsize=(16, 8))
-plt.subplot(1, 3, 1)
-plt.imshow(color_marked)
-plt.title("Farbbild (B04, B03, B02 – 20m)")
-plt.axis('off')
+rows = int(np.ceil(len(subplots) / 3))
 
-plt.subplot(1, 3, 2)
-plt.imshow(infrared)
-plt.title("Infrarotbild (B12, B11, B8A – 20m)")
-plt.axis('off')
+if fullscreen:
+    plt.get_current_fig_manager().full_screen_toggle()
+else:
+    plt.figure(figsize=(16, 8))
 
-plt.subplot(1, 3, 3)
-plt.imshow(fire_mask, cmap='gray')
-plt.title("Aktive Feuer-Pixel (weiß)")
-plt.axis('off')
+for i, subplot in enumerate(subplots, start=1):
+    plt.subplot(rows, 3, i)
+    plt.imshow(subplot.img, cmap=subplot.cmap)
+    plt.title(subplot.title)
+    plt.axis('off')
 plt.show()
