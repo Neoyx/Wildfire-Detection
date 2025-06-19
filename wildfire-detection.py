0ace6fbd3fceb31f7cc43e4fe7c97abd66641669
Neoyx <noah.leuft@gmail.com>
2025-06-19 23:07:58 +0200

    improved visualization and implemented edge detection for burned area

--- wildfire-detection.py
+++ wildfire-detection.py
@@ -12,116 +12,160 @@ from skimage import measure, morphology
 from skimage.exposure import rescale_intensity
 import sequential_regioning_cpp
 
-# Stacking und Normalisierung des Farbbildes
+# Stacking the image
 def stack_img(band_1, band_2, band_3):
-    img = np.dstack((band_1, band_2, band_3)) # Stacking, um aus 3 Graustufenbildern ein Farbbild zu machen
-    img /= np.percentile(img, 98) # Kontrast optimieren durch Normalisierung des Bildes
-    img = np.clip(img, 0, 1)
+    img = np.dstack((band_1, band_2, band_3)) # Stacking the bands
+    img /= np.percentile(img, 98) # Optimize contrast
+    img = np.clip(img, 0, 1) # Normalize
     return img
 
 
 def main(img: images.Image, plot_sync_zoom: bool = True):
     time_start = time.time()
 
-    b12_norm, b11_norm, b8a_norm, b04_norm, b03_norm, b02_norm, cloud_mask_norm = get_normalized_bands(img)
+    # Loading the bands
+    b12_norm, b11_norm, b8a_norm, b04_norm, b03_norm, b02_norm, cm_norm = get_normalized_bands(img)
 
+    # Stacking the bands
     infrared = stack_img(b12_norm, b11_norm, b8a_norm)
-    swir_composite = stack_img(b12_norm, b8a_norm, b04_norm) # SWIR Composite
-    nir_swir_composite = stack_img(b8a_norm, b11_norm, b04_norm) # NIR-SWIR Composite
-
     color = stack_img(b04_norm, b03_norm, b02_norm)
+    
 
-    # Detektion des Feuers
-    outer_fire_mask = (b12_norm > 0.6) & (b11_norm < 0.5) & (b8a_norm < 0.5) # & (cloud_mask == 0) # low b11 and b8a (G, B) to exclude clouds and vegetation
+    # 1. Fire-detection
+
+    # Low b11 and b8a (G, B) to exclude clouds and vegetation
+    #outer_fire_mask = (b12_norm > 0.5) & (b11_norm < 0.4) & (b8a_norm < 0.5)
+    outer_fire_mask = (b12_norm > 0.6) & (b11_norm < 0.5) & (b8a_norm < 0.5) 
     outer_fire_mask = outer_fire_mask.astype(np.uint16)
 
-    # dilate the fire to enlarge the detection radius for the core fire
+    # TODO: Suppression mode ==> Run filter above outer fire mask and check if it is a single pixel, then discard
+
+    # Dilate the fire to enlarge the detection radius for the core fire
     fire_closing = np.ones((135, 135), np.uint16)
-    #dilated_fire = cv2.dilate(outer_fire_mask.astype(np.uint8), filter, iterations=5)
     closed_fire_mask = cv2.morphologyEx(outer_fire_mask, cv2.MORPH_CLOSE, fire_closing)
 
-    # search for yellow/white fire-pixels near the red pixels
-    # & (b04_norm < 0.8) & (b03_norm < 0.8) & (b02_norm < 0.8) ==> Alternative zur cloud_mask == 0
-    core_fire_mask = (b12_norm > 0.7) & (b11_norm > 0.7) & (closed_fire_mask == 1) & (b04_norm < 0.8) & (b03_norm < 0.8) & (b02_norm < 0.8) # & (cloud_mask == 0)
+    # Search for yellow/white fire pixels near the red pixels
+    core_fire = (b12_norm > 0.7) & (b11_norm > 0.5) & (closed_fire_mask == 1) # searching for yellow fire pixels
+    cloud_filter = (b04_norm < 0.8) & (b03_norm < 0.8) & (b02_norm < 0.8) # filtering the clouds
+    core_fire_mask = core_fire & cloud_filter
     core_fire_mask = core_fire_mask.astype(np.uint16)
 
-    # TODO: Fehldetektierte rot-angestrahlte Wolken herausfiltern
+    # TODO: Problem ==> Fehldetektierte rot-angestrahlte Wolken herausfiltern
     # Problem an Anwendung von Cloudmask: Unter den Wolken liegende Feuer werden nicht mehr erkannt
-    # ==> Eventuelle Loesung: Cloud-Closing (siehe fortfolgend)
-
-    # apply another closing to fill the holes created by the cloudmask
-    #cloud_closing = np.ones((5, 5), np.uint8)
-    #closed_core_fire_mask = cv2.morphologyEx(core_fire_mask, cv2.MORPH_CLOSE, cloud_closing)
-    # Problem: Unter Wolken liegende Feuer können NICHT reproduziert werden
 
     # TODO: Groesse der Filtermasken evtl. je nach Groesse des Feuers dynamisch anpassen
 
+    # Storing the indices of the fire masks
     outer_fire_indices = np.where(outer_fire_mask)
     core_fire_indices = np.where(core_fire_mask)
 
+    # Mark the fire in the color image in red and yellow
+    color_marked = color.copy()
+    color_marked[outer_fire_indices[0], outer_fire_indices[1]] = [1, 0, 0] # Mark the outer fire in red
+    color_marked[core_fire_indices[0], core_fire_indices[1]] = [1, 1, 0] # Mark the core fire in yellow
+
+    # Combine the fire masks
     final_fire_mask = outer_fire_mask | core_fire_mask
 
-    # Markierung des Feuers im Farbbild in rot
-    color_marked = color.copy()
-    color_marked[outer_fire_indices[0], outer_fire_indices[1]] = [1, 0, 0]
-    color_marked[core_fire_indices[0], core_fire_indices[1]] = [1, 1, 0]
+    # Increasing the size of the fire marks for visualization
+    visual_dilatation = np.ones((50, 50), np.uint16)
+    final_fire_mask_dilated = cv2.morphologyEx(final_fire_mask.astype(np.uint8), cv2.MORPH_DILATE, visual_dilatation)
+    final_fire_indices = np.where(final_fire_mask_dilated)   
+    color_marked_dilated = color.copy()
+    color_marked_dilated[final_fire_indices[0], final_fire_indices[1]] = [1, 0, 0]
+    
 
-    # TODO: rot markierten Feuer-Pixel im Farbbild evtl. durch Dilatation oder andere Filter vergroessern
+    # 2. Regioning of the fires
 
-    kernel = np.ones((7,7),np.uint16)
-    combinedRegion_closed = cv2.morphologyEx(final_fire_mask, cv2.MORPH_CLOSE, kernel)
+    kernel_medium = np.ones((7,7),np.uint16)
+    combined_region_closed = cv2.morphologyEx(final_fire_mask, cv2.MORPH_CLOSE, kernel_medium)
 
-    kernel2 = np.ones((2,2),np.uint16)
-    combinedRegion_opened = cv2.morphologyEx(combinedRegion_closed, cv2.MORPH_OPEN, kernel2)
+    kernel_small = np.ones((2,2),np.uint16)
+    combined_region_opened = cv2.morphologyEx(combined_region_closed, cv2.MORPH_OPEN, kernel_small)
 
     # labeled_fire, amount_regions = sequential_regioning(combinedRegion_opened, n8=True) # using the pure Python implementation
-    labeled_fire, amount_regions  = sequential_regioning_cpp.run(combinedRegion_opened, n8=True) # using the C++ implementation for performance
+    labeled_fire, amount_regions  = sequential_regioning_cpp.run(combined_region_opened, n8=True) # using the C++ implementation for performance
 
-    #burn_index2 = b12_norm - 0.1 * (b11_norm + b8a_norm) # TODO: test different weights
-    burn_index2 = b12_norm - (0.7 * b11_norm) # b8a bringt nicht viel, da wolken/rauch reflektion brandfäche verdeckt
+
+    # 3. Detecting the burned area
+
+    # Converting the bands to greyscale by dividing b12 with b11 for brightening the burned area
+    burn_index = b12_norm / (b11_norm + 1e-6) # added constant denominator to avoid dividing by zero
 
     # Normalize burn_index to 0–1
-    burn_index2 = np.clip((burn_index2 - burn_index2.min()) / (burn_index2.max() - burn_index2.min()), 0, 1)
+    burn_index = (burn_index - burn_index.min()) / (burn_index.max() - burn_index.min() + 1e-6)
 
-    burn_index2[b11_norm < 0.2] = burn_index2.mean() # filtering the water
-    burn_index2 = burn_index2**0.2 # gamma correction
+    # Filtering sharp edges that aren't part of the burned area
+    mean_mask = ((burn_index > 0) & (b11_norm >= 0.2)) # avoid black areas or water for computing the mean
+    mean = burn_index[mean_mask].mean()
+    burn_index[b11_norm < 0.2] = mean     # filtering water
+    burn_index[b8a_norm > 0.5] = mean     # filtering vegetation
+    burn_index[b11_norm > 0.3] = mean     # filtering bright areas
 
-    smoothed = gaussian_filter(burn_index2, sigma=3.0)  # try 1.0 to 3.0
-    sobelx = cv2.Sobel(smoothed, cv2.CV_64F, 1, 0, ksize=3)
-    sobely = cv2.Sobel(smoothed, cv2.CV_64F, 0, 1, ksize=3)
+    # Gamma correction to make the image brighter
+    burn_index = burn_index**0.5 
 
-    edges = np.hypot(sobelx, sobely)
-    edges = cv2.normalize(edges, None, 0, 1, cv2.NORM_MINMAX)
+    # Applying sobel operator for edge detection of the burned area
+    smoothed = gaussian_filter(burn_index, sigma=2.0) # applying gaussian filter to smooth small edges
+    sobelx = cv2.Sobel(smoothed, cv2.CV_64F, 1, 0, ksize=3) # in x-direction
+    sobely = cv2.Sobel(smoothed, cv2.CV_64F, 0, 1, ksize=3) # in y-direction
+    edges_sobel = np.hypot(sobelx, sobely) # combine filters
+    edges_sobel = cv2.normalize(edges_sobel, None, 0, 1, cv2.NORM_MINMAX) # normalize
+    binary_edges_sobel = edges_sobel > np.percentile(edges_sobel, 98) # only keep strongest edges
 
-    # Optional threshold
-    binary_edges = edges > 0.1  # or another threshold
-    cleaned_edges = morphology.remove_small_objects(binary_edges, min_size=300)
+    # Applying canny operator for detecting sharp edges only
+    edges_canny = canny(burn_index, sigma=2.0, low_threshold=np.percentile(edges_sobel, 90), high_threshold=np.percentile(edges_sobel, 95)) 
+    dilated_edges = cv2.morphologyEx(edges_canny.astype(np.uint8), cv2.MORPH_DILATE, kernel_medium)
 
-    #edges2 = canny(burn_index2, sigma=2.0)
-    edges2 = canny(burn_index2, sigma=2.0, low_threshold=0.001, high_threshold=0.08) 
-    dilated_edges = cv2.morphologyEx(edges2.astype(np.uint8), cv2.MORPH_DILATE, kernel)
+    # Dilate detected edges by canny in order to limit the detection radius of sobel
+    area_dilatation = np.ones((200, 200), np.uint16)
+    dilated_edges_canny = cv2.morphologyEx(dilated_edges.astype(np.uint8), cv2.MORPH_DILATE, area_dilatation)
+
+    combined_edges = binary_edges_sobel & dilated_edges_canny # Keep all sobel pixels that overlap dilated canny area
+    combined_edges_closed = cv2.morphologyEx(combined_edges, cv2.MORPH_CLOSE, kernel_medium) # Close holes in burned area
+    combined_edges_opened = cv2.morphologyEx(combined_edges_closed, cv2.MORPH_OPEN, kernel_small) # Remove artifacts
+
+    # Computing the size of the burning area
+    number_fire_pixels = np.count_nonzero(final_fire_mask)
+    burning_area = 400 * number_fire_pixels / 1000000
+    print(f"Size of active fire area: {burning_area}km^2")
 
     time_end = time.time()
     print(f"Complete Processing time: {time_end - time_start:.2f} seconds")
 
 
     subplots_data = [
+        Subplot("Farbbild (B04, B03, B02 – 20m)", color),
         Subplot("Farbbild (makiert) (B04, B03, B02 – 20m)", color_marked),
+        Subplot("Farbbild (makiert - groß) (B04, B03, B02 – 20m)", color_marked_dilated),
         Subplot("Infrarotbild (B12, B11, B8A – 20m)", infrared),
-        #Subplot("Aktive Feuer-Pixel (weiß)", final_fire_mask, cmap='gray'),
+        Subplot("Aktive Feuer-Pixel (weiß)", final_fire_mask, cmap='gray'),
         #Subplot("Kombiniertes Feuer (Closed))", combinedRegion_closed, cmap='gray'),
         #Subplot("Kombiniertes Feuer (Closed-Open)", combinedRegion_opened, cmap='gray'),
         Subplot(f"Regionenmarkiertes Feuer | Regions: {amount_regions}", labeled_fire, cmap='gray'),
-        Subplot("Verbrannte Flaeche (Bänder)", burn_index2, cmap='gray'),
-        Subplot("Kanten (Sobel)", cleaned_edges, cmap='gray'),
-        Subplot("Kanten (Canny)", dilated_edges, cmap='gray')
+        #Subplot("Verbrannte Fläche", burn_index, cmap='gray'),
+        Subplot("Verbrannte Fläche (Sobel)", binary_edges_sobel, cmap='gray'),
+        Subplot("Verbrannte Fläche (Canny)", dilated_edges, cmap='gray'),
+        Subplot("Verbrannte Fläche (kombiniert)", combined_edges_opened, cmap='gray'),
+        #Subplot("b12_norm", b12_norm, cmap='hot'),
+        #Subplot("b11_norm", b11_norm, cmap='hot'),
+        #Subplot("b8a_norm", b8a_norm, cmap='hot'),
     ]
+
+    # subplots_data_2 = [
+    #     Subplot("color", color, cmap='hot'),
+    #     Subplot("infrared", infrared, cmap='hot'),
+    #     Subplot("burn index", burn_index, cmap='hot'),
+    #     Subplot("b12_norm", b12_norm, cmap='hot'),
+    #     Subplot("b11_norm", b11_norm, cmap='hot'),
+    #     Subplot("b8a_norm", b8a_norm, cmap='hot'),
+    # ]
     
     visualisation.plot(subplots_data, plot_sync_zoom=plot_sync_zoom)
 
 
 if __name__ == "__main__":
     main(
-        images.Flin_Flon,  # Change to any image from the images module
+        images.Park_Fire_1,  # Change to any image from the images module
         plot_sync_zoom=True  # Set to False to disable synchronized zooming
     )
